0109f8914580f4efdae6fa6efe258e9ca2ee193a
Alex Velasquez <alexvelasquez94@gmail.com>
2023-09-21 23:40:42 -0300

    [FEAT] libreries vee-validate and vue-icons

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
 import { router } from './router'
+
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import * as FaIcons from "oh-vue-icons/icons/fa";
+const Fa = Object.values({ ...FaIcons });
+addIcons(...Fa);
+
 const app = createApp(App)
 app.use(router)
+app.component("v-icon", OhVueIcon);
 app.mount('#app')
