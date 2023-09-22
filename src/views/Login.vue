c2ae7e28f36544db0152b11a6f1bec941a6e1695
Alex Velasquez <alexvelasquez94@gmail.com>
2023-09-22 02:09:36 -0300

    [FEAT]  add attributes dom

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -13,7 +13,7 @@
         </h1>
         <p></p>
 
-        <form @submit.prevent="auth">
+        <form @submit.prevent="login">
           <div class="mb-6">
             <label for="email" class="block mb-2 text-sm text-gray-600"
               >Email</label
@@ -25,34 +25,32 @@
               name="email"
               class="text-sm w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-1 focus:ring-cyan-500"
             />
-            <div class="text-red-500 text-xs">{{ errors.username }}</div>
+            <div id="error-username" class="text-red-500 text-xs">{{ errors.username }}</div>
           </div>
           <div class="mb-6">
             <label for="password" class="block mb-2 text-sm text-gray-600"
               >Password</label
             >
-            <div
-              x-data="{ show: false }"
-              class="relative flex items-center mt-2"
-            >
+            <div class="relative flex items-center mt-2">
               <input
                 v-bind="password"
                 :type="showPassword ? 'text' : 'password'"
-                id="current-password"
-                name="current-password"
+                id="password"
+                name="password"
                 class="text-sm w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-1 focus:ring-cyan-500"
               />
               <button
+                id="hide-show-password"
                 type="button"
                 class="absolute right-2 bg-transparent flex items-center justify-center hover:text-blue-600"
                 @click="showPassword = !showPassword"
               >
-                <v-icon v-if="!showPassword" name="fa-eye" />
-                <v-icon v-else name="fa-eye-slash" />
+                <v-icon v-if="!showPassword" name="fa-eye" id="show-password"/>
+                <v-icon v-else name="fa-eye-slash" id="hide-password"/>
               </button>
             </div>
 
-            <div class="text-red-500 text-xs">{{ errors.password }}</div>
+            <div id="error-password" class="text-red-500 text-xs">{{ errors.password }}</div>
           </div>
           <div>
             <input
@@ -61,12 +59,12 @@
               type="checkbox"
               class="bg-white text-purple mr-2 mt-[-2px]"
             />
-            <label class="text-sm" for="remember">Remember</label>
+            <label class="text-sm remember" for="remember">Remember</label>
           </div>
           <button
             type="submit"
-            :disabled=" !meta.valid"
-            class="w-full bg-gradient-to-r bg-cyan-600 text-white py-2 rounded-lg mx-auto block mt-4 mb-6"
+            :disabled="!meta.valid"
+            class="submit w-full bg-gradient-to-r bg-cyan-600 text-white py-2 rounded-lg mx-auto block mt-4 mb-6"
           >
             <div class="flex justify-center gap-2">
               <p>loggin in</p>
@@ -80,14 +78,14 @@
 <script setup>
 import { reactive, ref, toRaw } from "vue";
 import { useAuth } from "../composables/useAuth";
-import { useRouter } from "vue-router";
-const router = useRouter();
-const { auth, errors, meta, errorMessage, defineInputBinds} = useAuth();
+const { login, errors, meta, errorMessage, defineInputBinds } = useAuth();
 
+defineProps({
+  title: String
+})
 const showPassword = ref(false);
 const username = defineInputBinds("username");
 const password = defineInputBinds("password");
-
 </script>
 <style>
 button:disabled,
