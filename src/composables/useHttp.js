6ae15d392d0018526be38438b948813592f37741
Alex Velasquez <alexvelasquez94@gmail.com>
2023-09-22 02:51:44 -0300

    [FIX] enviroment variable

--- src/composables/useHttp.js
+++ src/composables/useHttp.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const http = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: import.meta.env.VITE_API_ENDPOINT
 });
 export const useHttp = () => {
     const post = (url, params) => {
